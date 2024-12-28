98c519baa218a17dfc8870b0c20eac25a833e0b8
BallahNet <44912400+AutohRap@users.noreply.github.com>
2024-12-28 03:11:11 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,6 +8,4 @@ resource "aws_instance" "myec2" {
     ami = "ami-0182f373e66f89c85" #according to region
     instance_type = "t2.micro" #instance type
     #instance properties:
-    /*tags = {
-        Name = " "
-    }*/
+ }

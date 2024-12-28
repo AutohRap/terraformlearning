8e7fe79ec53a0c299b47cb6aa102de1141e99d1b
BallahNet <44912400+AutohRap@users.noreply.github.com>
2024-12-28 02:47:26 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+#Configure AWS Provider:
+provider "aws" {
+  region     = "us-east-1" #region according to cloud provider
+  
+}
+#instance creation:
+resource "aws_instance" "myec2" {
+    ami = "ami-0182f373e66f89c85" #according to region
+    instance_type = "t2.micro" #instance type
+    #instance properties:
+    /*tags = {
+        Name = " "
+    }*/
